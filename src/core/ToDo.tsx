a7595c37b6d57805cc7c9b8c3b4200be6f842b69
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-02 21:34:40 -0300

    feat: localStorage functionality

--- src/core/ToDo.tsx
+++ src/core/ToDo.tsx
@@ -31,6 +31,26 @@ class ToDo {
 		return this.#createdAt;
 	}
 
+	// Função utilizada por JSON.stringify().
+	// JSON.stringfy() utiliza uma função toJSON() do objeto caso ela exista.
+	// Para mais detalhes: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#tojson_behavior.
+	// Aqui, passo um objeto JavaScript para o JSON.stringfy(), associo cada atributo do objeto com o atributo correspodente da classe ToDo.
+	public toJSON(): string {
+		return JSON.stringify({
+			id: this.id,
+			description: this.description,
+			done: this.done,
+			createdAt: this.createdAt
+		});
+	}
+
+	// Funçao que retorna um objeto do tipo ToDo bease em uma string JSON recebida como argumento.
+	public static fromJSON(serialized: string): ToDo {
+		const todo = JSON.parse(serialized);
+		// Note que devemos passar todo.createdAt como argumento para criação de um novo objeto Date.
+		// Isso deve ser feito pois na conversão para JSON, todo o objeto Date foi serializado.
+		return new ToDo(todo.description, todo.done, new Date(todo.createdAt), todo.id);
+	}
 }
 
 export default ToDo;
--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -7,20 +7,50 @@ import styles from './ToDo.module.css';
 import ToDo from '../core/ToDo';
 
 const Home: React.FC = () => {
+	const [todos, setTodos] = useState<Array<ToDo>>(new Array<ToDo>());
+	// Apenas para implementação inicial.
+	// Este contador é utilizado para gerar IDs para os items da lista.
+	const [counter, setCounter] = useState<number>(0);
 
-	const [todos, setTodos] = useState<ToDo[]>([]);
-	// Just for initial implementation.
-	// This counter is used do generate ids for the todos in the list.
-	const [count, setCount] = useState<number>(0);
-
-	// Increment the counter when the todo list is updated.
 	useEffect(() => {
-		setCount(count + 1);
+		// Quando geramos uma string a partir do array de objetos do tipo ToDo, temos o seguinte resultado:
+		// ["{}", "{}", "{}", ...]
+		// Cada objeto se torna uma string e o mesmo vale para objetos dentro do objeto, quero dizer:
+		// - Um objeto dentro do array se torna um json, onde cada par atributo/valor vira uma string.
+		// - No objeto do tipo ToDo temos um atributo que é um objeto do tipo Date, ele sofre o mesmo efeito.
+		// Devido a isso, ao obter o array do localStorage, e converte-lo com JSON.parse(), temos um array de strings (os objetos).
+		// Feito isso, percorremos este array de string, e utilizamos a função fromJSON() que implementei.
+		// Esta função retorna um objeto do tipo ToDo baseado na string recebida.
+		const stringifiedArray: string[] = JSON.parse(localStorage.getItem('todos') || '[]');
+		stringifiedArray.forEach(todo => {
+			setTodos(todos => [...todos, ToDo.fromJSON(todo)]);
+		});
+
+		// Obtenho o contador armazenado no localStorage.
+		const counter: number = JSON.parse(localStorage.getItem('counter') || '0');
+		setCounter(counter);
+	}, []);
+
+	useEffect(() => {
+		// Sempre que o array de ToDos é atualizado, devemos atualizar localStorage com o novo conteúdo.
+		// JSON.stringify() ira serializar o array de todos e ira notar que cada objeto do array tem uma funçao toJSON().
+		// Desse modo, JSON.stringify() irá utilizar a função toJSON() implementada para serializar cada objeto do array.
+		localStorage.setItem('todos', JSON.stringify(todos));
+		// Se em algum momento não houver todos no array, podemos zerar o contador com segurança.
+		// Com segurança quer dizer que não corremos risco de zerá-lo e causar conflito de ids.
+		if (todos.length == 0)
+			setCounter(0);
 	}, [todos]);
 
+	useEffect(() => {
+		// Atualiza o contador no localStorade sempre que ele muda.
+		localStorage.setItem('counter', JSON.stringify(counter));
+	}, [counter]);
+
 	const addTodo = (todo: string): void => {
-		const newTodo = new ToDo(todo, false, new Date(), count.toString());
+		const newTodo = new ToDo(todo, false, new Date(), counter.toString());
 		setTodos(todos => [...todos, newTodo]);
+		setCounter(counter + 1);
 	};
 
 	const deleteToDo = ({ id }: ToDo): boolean => {
