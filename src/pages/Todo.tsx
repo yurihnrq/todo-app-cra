dff28f094bf9a8707f8668f6254b53a0e1ec777b
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-10 10:10:18 -0300

    update: TodoRepo and useTodos code refactor

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -8,12 +8,13 @@ import useTodos from '../hooks/useTodos';
 
 const Home: React.FC = () => {
 	const { user } = useAuth();
-	const { todos, addTodo, deleteTodo, updateTodo } = useTodos(user);
+	const { todos, error, addTodo, deleteTodo, updateTodo } = useTodos(user);
 
 	return (
 		<Layout>
 			<section className={styles.Todo}>
 				<h2 className='dark:text-slate-200'>Olá {user?.email?.slice(0, user?.email?.indexOf('@'))}</h2>
+				{error ? (<span className={styles.warning}>{error}</span>) : (null)}
 				<Form addAction={addTodo} />
 				<Table deleteAction={deleteTodo} updateAction={updateTodo} todos={todos} />
 			</section>
