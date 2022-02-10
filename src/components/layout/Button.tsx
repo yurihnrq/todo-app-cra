dd94cd74070c82e9a283308bc0578712917d4f26
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-09 23:26:36 -0300

    update: buttons focus shadow

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -10,23 +10,17 @@ interface IButton {
 }
 
 const Button: React.FC<IButton> = ({ className, children, label, color, onClick, disabled }) => {
+
 	return (
 		<button
-			className={`${styles.Button} ${className} ${colors[color]}`}
+			className={`${styles.Button} ${className} 
+			${color === 'blue' ? styles.blue : color === 'red' ? styles.red : styles.green}`}
 			aria-label={label} onClick={onClick} disabled={disabled}
 		>
 			{children}
 		</button>
 	);
 };
-
-
-const colors = {
-	'red': 'from-red-700 to-orange-600 hover:from-red-600 hover:to-orange-500 active:from-red-800 active:to-orange-700',
-	'blue': 'from-blue-700 to-cyan-600 hover:from-blue-600 hover:to-cyan-500 active:from-blue-800 active:to-cyan-700',
-	'green': 'from-green-700 to-emerald-600 hover:from-green-600 hover:to-emerald-500 active:from-green-800 active:to-emerald-700',
-};
-
 
 Button.defaultProps = {
 	className: '',
