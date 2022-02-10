a6c4af54a71943981f76b34fc589d0f09bc360d8
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-10 11:19:11 -0300

    update: improved useTodos performance

--- src/core/TodoRepo.ts
+++ src/core/TodoRepo.ts
@@ -2,7 +2,7 @@ import Todo from './Todo';
 import { User } from 'firebase/auth';
 
 interface TodoRepo {
-	save(todo: Todo, user: User | null): Promise<void>;
+	save(todo: Todo, user: User | null): Promise<Todo>;
 	delete(todo: Todo, user: User | null): Promise<void>;
 	update(todo: Todo, user: User | null): Promise<void>
 	getAll(user: User | null): Promise<Todo[]>;
