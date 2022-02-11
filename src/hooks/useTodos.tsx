6c284c0cc5f14b95c155b1d1180568842e0035e0
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-11 20:36:08 -0300

    fix: better offline experience

--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -29,58 +29,44 @@ const useTodos = (user: User | null): useTodosReturnTypes => {
 			dataCollection.getAll(user).then(todos => {
 				setTodos(todos);
 			});
-			if (error !== null ) setError(null);
-		} catch (error) {
-			setError('Erro: ' + error);
+			if (error !== null) setError(null);
+		} catch (err) {
+			setError('Erro: ' + err);
+			console.error(error);
 		}
 	};
 
 	const addTodo = (todo: string) => {
 		try {
 			const todoObj = new Todo(todo, false, new Date());
-			dataCollection.save(todoObj, user).then(todo => {
-				// Aqui eu mudo o estado diretamente ao invés de chamar o método
-				// getTodos, pois assim o delay entre a ação e o retorno visual é menor.
-
-				// Como estamos em um bloco try catch, entrar neste then() significa
-				// que todo o provesso de adicionar o documento no Firestore ocorreu com sucesso.
-				setTodos(prevState => [todo, ...prevState]);
-			});
-			if (error !== null ) setError(null);
-		} catch (error) {
-			setError('Erro: ' + error);
+			dataCollection.save(todoObj, user);
+			getTodos();
+			if (error !== null) setError(null);
+		} catch (err) {
+			setError('Erro: ' + err);
+			console.error(error);
 		}
 	};
 
 	const deleteTodo = (todo: Todo) => {
 		try {
-			dataCollection.delete(todo, user).then(() => {
-				const todosHandler = [...todos];
-				const index = todosHandler.findIndex(elem => elem.id == todo.id);
-				todosHandler.splice(index , 1);
-				// setTodos aqui pelo menos motivo explicado em addTodo();
-				setTodos([...todosHandler]);
-			});
-			if (error !== null ) setError(null);
-		} catch (error) {
-			setError('Erro: ' + error);
+			dataCollection.delete(todo, user);
+			getTodos();
+			if (error !== null) setError(null);
+		} catch (err) {
+			setError('Erro: ' + err);
+			console.error(error);
 		}
 	};
 
 	const updateTodo = (todo: Todo) => {
 		try {
-			dataCollection.update(todo, user).then(() => {
-				const todosHandler = [...todos];
-				const index = todosHandler.findIndex(elem => elem.id == todo.id);
-				// update() recebe o objeto Todo atualizado. Desse modo,
-				// devemos apenas substitui-lo no index correto.
-				todosHandler[index] = todo;
-				// setTodos aqui pelo menos motivo explicado em addTodo();
-				setTodos([...todosHandler]);
-			});
-			if (error !== null ) setError(null);
-		} catch (error) {
-			setError('Erro: ' + error);
+			dataCollection.update(todo, user);
+			getTodos();
+			if (error !== null) setError(null);
+		} catch (err) {
+			setError('Erro: ' + err);
+			console.error(error);
 		}
 	};
 
