74bb7cbf3016ffa0bce197cfbf7465b218f3267b
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-22 21:37:02 -0300

    update: better Header width

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,98 +13,85 @@ const Header: React.FC = () => {
 
   return (
     <header className={styles.Header}>
-      <Link to='/' className={styles.logo}>
-        <div>
-          {CheckIcon}
-          <span>ToDo App</span>
-        </div>
-      </Link>
-      <div className={styles.buttonContainer}>
-        <button
-          className='p-0 sm:mr-4'
-          aria-label='Change theme'
-          onClick={() =>
-            theme === 'light' ? setTheme('dark') : setTheme('light')
-          }
-        >
-          {theme === 'light' ? MoonIcon : SunIcon}
-        </button>
-        <button
-          aria-label='Open mobile navbar'
-          onClick={() => setSideNav(!sideNav)}
-        >
-          {MenuIcon}
-        </button>
-      </div>
-      <nav className={styles.nav}>
-        <button
-          className='p-0 sm:mr-4'
-          aria-label='Change theme'
-          onClick={() =>
-            theme === 'light' ? setTheme('dark') : setTheme('light')
-          }
-        >
-          {theme === 'light' ? MoonIcon : SunIcon}
-        </button>
-        <Link to='/'>Inicio</Link>
-        <Link to='/sobre'>Sobre</Link>
-        {user ? (
-          <Link
-            to='/login'
-            onClick={() => {
-              if (logout) logout();
-            }}
-          >
-            Sair
-          </Link>
-        ) : (
-          <Link to='/login'>Login</Link>
-        )}
-      </nav>
-      <CSSTransition
-        in={sideNav}
-        timeout={150}
-        unmountOnExit
-        classNames={{
-          enter: styles.sideNavEnter,
-          enterActive: styles.sideNavEnterActive,
-          exit: styles.sideNavExit,
-          exitActive: styles.sideNavExitActive,
-        }}
-      >
-        <div className={styles.sideNav}>
+      <div className={styles.container}>
+        <Link to='/' className={styles.logo}>
           <div>
-            <button
-              aria-label='Close mobile navbar'
-              onClick={() => setSideNav(!sideNav)}
-            >
-              {CloseIcon}
-            </button>
+            {CheckIcon}
+            <span>ToDo App</span>
           </div>
-          <nav>
-            <Link to='/' onClick={() => setSideNav(!sideNav)}>
-              Inicio
-            </Link>
-            <Link to='/sobre' onClick={() => setSideNav(!sideNav)}>
-              Sobre
-            </Link>
-            {user ? (
-              <Link
-                to='/login'
-                onClick={() => {
-                  if (logout) logout();
-                }}
-              >
-                Sair
-              </Link>
-            ) : (
-              <Link to='/login' onClick={() => setSideNav(!sideNav)}>
-                Login
-              </Link>
-            )}
-          </nav>
+        </Link>
+        <div className={styles.buttonContainer}>
+          <button
+            className='p-0 sm:mr-4'
+            aria-label='Change theme'
+            onClick={() => (theme === 'light' ? setTheme('dark') : setTheme('light'))}>
+            {theme === 'light' ? MoonIcon : SunIcon}
+          </button>
+          <button aria-label='Open mobile navbar' onClick={() => setSideNav(!sideNav)}>
+            {MenuIcon}
+          </button>
         </div>
-      </CSSTransition>
+        <nav className={styles.nav}>
+          <button
+            className='p-0 sm:mr-4'
+            aria-label='Change theme'
+            onClick={() => (theme === 'light' ? setTheme('dark') : setTheme('light'))}>
+            {theme === 'light' ? MoonIcon : SunIcon}
+          </button>
+          <Link to='/'>Inicio</Link>
+          <Link to='/sobre'>Sobre</Link>
+          {user ? (
+            <Link
+              to='/login'
+              onClick={() => {
+                if (logout) logout();
+              }}>
+              Sair
+            </Link>
+          ) : (
+            <Link to='/login'>Login</Link>
+          )}
+        </nav>
+        <CSSTransition
+          in={sideNav}
+          timeout={150}
+          unmountOnExit
+          classNames={{
+            enter: styles.sideNavEnter,
+            enterActive: styles.sideNavEnterActive,
+            exit: styles.sideNavExit,
+            exitActive: styles.sideNavExitActive
+          }}>
+          <div className={styles.sideNav}>
+            <div>
+              <button aria-label='Close mobile navbar' onClick={() => setSideNav(!sideNav)}>
+                {CloseIcon}
+              </button>
+            </div>
+            <nav>
+              <Link to='/' onClick={() => setSideNav(!sideNav)}>
+                Inicio
+              </Link>
+              <Link to='/sobre' onClick={() => setSideNav(!sideNav)}>
+                Sobre
+              </Link>
+              {user ? (
+                <Link
+                  to='/login'
+                  onClick={() => {
+                    if (logout) logout();
+                  }}>
+                  Sair
+                </Link>
+              ) : (
+                <Link to='/login' onClick={() => setSideNav(!sideNav)}>
+                  Login
+                </Link>
+              )}
+            </nav>
+          </div>
+        </CSSTransition>
+      </div>
     </header>
   );
 };
