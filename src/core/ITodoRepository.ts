e961abb714ef0ed433c5e1a9b0de40744291ce21
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-17 19:37:02 -0300

    refactor: better firebase config and repository naming

--- src/core/ITodoRepository.ts
+++ src/core/ITodoRepository.ts
@@ -1,11 +1,9 @@
 import Todo from './Todo';
 import { User } from 'firebase/auth';
 
-interface TodoRepo {
+export interface ITodoRepository {
 	save(todo: Todo, user: User | null): Promise<Todo>;
 	delete(todo: Todo, user: User | null): Promise<void>;
 	update(todo: Todo, user: User | null): Promise<void>;
 	getAll(user: User | null): Promise<Todo[]>;
 }
-
-export default TodoRepo;
