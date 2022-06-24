5632eb0b16c07e8cdcace4622effbed1532aee5b
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-24 10:48:54 -0300

    feat: confirmation modal at categories deletion

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/base/Button';
 import { TrashIcon } from '../components/base/Icons';
+import Modal from '../components/base/Modal';
 import Table from '../components/base/Table';
 import Layout from '../components/Layout';
 import { useTodoContext } from '../context/TodoContext';
@@ -8,11 +9,13 @@ import styles from './styles/Categories.module.css';
 
 const Categories: React.FC = () => {
   const { categories, deleteCategory } = useTodoContext();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [categoryToDelete, setCategoryToDelete] = useState<string | null>(null);
 
   return (
     <Layout>
       <section>
-        <Table className={styles.CategoriesTable} caption='Categorias'>
+        <Table className={styles.categoriesTable} caption='Categorias'>
           <tbody>
             {categories.map(category =>
               category !== 'default' ? (
@@ -22,7 +25,10 @@ const Categories: React.FC = () => {
                     <Button
                       color='red'
                       label='Delete ToDo'
-                      onClick={() => deleteCategory(category)}>
+                      onClick={() => {
+                        setCategoryToDelete(category);
+                        setIsModalOpen(true);
+                      }}>
                       <TrashIcon />
                     </Button>
                   </td>
@@ -32,6 +38,30 @@ const Categories: React.FC = () => {
           </tbody>
         </Table>
       </section>
+      {isModalOpen ? (
+        <Modal title='Exclusão' onClose={() => setIsModalOpen(false)}>
+          <p>
+            Tem certeza que deseja excluir <strong>{categoryToDelete}</strong>?
+          </p>
+          <div className={styles.buttonContainer}>
+            <Button
+              color='red'
+              label='Excluir categoria'
+              onClick={() => {
+                if (categoryToDelete) {
+                  deleteCategory(categoryToDelete);
+                  setCategoryToDelete(null);
+                }
+                setIsModalOpen(false);
+              }}>
+              Excluir
+            </Button>
+            <Button color='blue' label='Cancelar exclusão' onClick={() => setIsModalOpen(false)}>
+              Cancelar
+            </Button>
+          </div>
+        </Modal>
+      ) : null}
     </Layout>
   );
 };
